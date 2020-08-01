dfe89d190fae71f8d518ecda313474027e9b200c
Tom Spencer <tom@homeflow.co.uk>
2020-08-01 14:06:27 +0100

    Add parameterized tests to refactor tests

--- src/test/java/com/codurance/StringCalculatorShould.java
+++ src/test/java/com/codurance/StringCalculatorShould.java
@@ -15,25 +15,17 @@ public class StringCalculatorShould {
   @CsvSource(value = {
           "''; 0",
           "0, 0; 0",
-          "1, 0; 1"
+          "1, 0; 1",
+          "1,2,3,4,5,6,7,8,9; 45",
+          "1001, 2; 2",
+          "'1\n2,3'; 6",
+          "'//;\n1;2'; 3",
+          "'//[***]\n1***2***3'; 6",
+          "'//[foo][bar]\n1foo2bar3'; 6",
+          "'//[*][%]\n1*2%3'; 6"
   }, delimiter = ';')
   void return_correct_addition_output_for_string_numbers(String input, int output) {
     assertEquals(output, stringCalculator.add(input));
-  }
-
-  @Test
-  void add_an_arbitrary_amount_of_numbers() {
-    assertEquals(45, stringCalculator.add("1,2,3,4,5,6,7,8,9"));
-  }
-
-  @Test
-  void returns_correct_output_with_newline_input() {
-    assertEquals(6, stringCalculator.add("1\n2,3"));
-  }
-
-  @Test
-  void returns_correct_output_with_custom_separators() {
-    assertEquals(3, stringCalculator.add("//;\n1;2"));
   }
 
   @Test
@@ -41,25 +33,5 @@ public class StringCalculatorShould {
     assertThrows(IllegalArgumentException.class, () -> {
       stringCalculator.add("1,-2,-3");
     });
-  }
-
-  @Test
-  void ignore_numbers_more_than_1000() {
-    assertEquals(2, stringCalculator.add("1001, 2"));
-  }
-
-  @Test
-  void adds_numbers_with_arbitrary_length_separators() {
-    assertEquals(6, stringCalculator.add("//[***]\n1***2***3"));
-  }
-
-  @Test
-  void adds_numbers_with_multiple_single_separators() {
-    assertEquals(6, stringCalculator.add("//[*][%]\n1*2%3"));
-  }
-
-  @Test
-  void returns_correct_result_for_multiple_longer_length_separators() {
-    assertEquals(6, stringCalculator.add("//[foo][bar]\n1foo2bar3"));
   }
 }
